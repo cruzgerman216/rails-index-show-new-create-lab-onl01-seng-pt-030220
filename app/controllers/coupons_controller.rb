79adca83adf6ecdc4679938057d4a47fc5cca5f0
German Cruz <cruzgerman216@yahoo.com>
2020-07-08 01:22:28 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,6 @@
 class CouponsController < ApplicationController
   def create
-    @coupon = Coupon.new 
+    @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
     @coupon.save
